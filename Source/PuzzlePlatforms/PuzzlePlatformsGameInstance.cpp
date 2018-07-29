887f551ad022994411ee66967f3fe81e72fa3057
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-07-29 03:04:14 -0400

    Console commands with Exec

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -1,6 +1,7 @@
 // Copyright 2018 Jeffrey Polasz. All Rights Reserved.
 
 #include "PuzzlePlatformsGameInstance.h"
+#include "Engine/Engine.h"
 
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer)
 {
@@ -10,4 +11,20 @@ UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitiali
 void UPuzzlePlatformsGameInstance::Init()
 {
 	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(-1, 2, FColor::Green, TEXT("Hosting!"));
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(-1, 5, FColor::Green, FString::Printf(TEXT("Joining %s."), *Address));
 }
